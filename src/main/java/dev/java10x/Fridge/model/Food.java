490c7fabc83758411d41e5473770d8897731da1e
S㉿_117 <brunoferreira1santos@gmail.com>
2025-10-17 17:45:05 -0300

    add Food entity with JPA annotations and Lombok integration

--- src/main/java/dev/java10x/Fridge/model/Food.java
+++ src/main/java/dev/java10x/Fridge/model/Food.java
@@ -1,10 +1,19 @@
 package dev.java10x.Fridge.model;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "food_table")
+@Data
 public class Food {
 
-    private
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Integer quantity;
+    private LocalDate expirationDate;
+
 }
